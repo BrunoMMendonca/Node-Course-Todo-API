5c91f806fa96899a129ab47c16c0dd8edd9608a4
Bruno Mendonça <bruno.m.mendonca@gmail.com>
2018-06-19 14:02:29 +0100

    Add tests to GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,12 +3,16 @@ const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {ObjectID} = require('mongodb');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'Get the dog to the vet'
     }, {
+    _id: new ObjectID(),
     text: 'Cook dinner'
     }, {
+    _id: new ObjectID(),
     text: 'Have fun'
     }];
 
@@ -69,4 +73,32 @@ describe('GET /todos', () => {
             .end(done);
     });
 
+});
+
+describe('GET /todos/:id', () =>{
+    it('should return todo doc',(done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done)=>{
+        var hexID = new ObjectID();
+        request(app)
+            .get(`/todos/${hexID.toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if not object id', (done)=>{
+        var hexID = new ObjectID();
+        request(app)
+            .get(`/todos/123`)
+            .expect(404)
+            .end(done);
+    });
 });
