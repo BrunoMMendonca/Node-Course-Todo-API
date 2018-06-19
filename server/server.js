c28f19e25a5cae5f4921f01bd1bd0e05f6c47670
Bruno Mendonça <bruno.m.mendonca@gmail.com>
2018-06-19 03:02:54 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -25,23 +26,28 @@ app.listen(3000,()=>{
     console.log('Connected to port 3000');
 });
 
-app.get('/todos',(req,res)=>{
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    };
+
+    Todo.findById(id).then((todo)=>{
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(404).send();
+    });
+});
+
+app.get('/todos',(req,res) => {
     Todo.find().then((todos)=>{
         res.send({todos});
     },(e)=>{
         res.status(400).send(e);
-    });
-});
-
-app.post('/users',(req, res)=>{
-    var user = new User({
-        email: req.body.email
-    });
-
-    user.save().then((doc)=> {
-        res.send(doc);
-    },(err)=>{
-        res.status(400).send(err);
     });
 });
 
